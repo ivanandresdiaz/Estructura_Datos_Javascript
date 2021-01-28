f98dc8b18f2544821de9e8dff46ac7ca7e00e707
ivan diaz <ivanandresdilox@gmail.com>
2021-01-28 10:23:19 -0500

    singly linked-list terminado

--- Linked-list/link.js
+++ Linked-list/link.js
@@ -1,36 +1,105 @@
-let singlyLinkedList = {
-  head: {
-    value: 1,
-    next: {
-      value: 2,
-      next: {
-        value: 3,
-        next: {
-          value: 4,
-          next: null,
-        },
-      },
-    },
-  },
-};
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
 
+// class MySinglyLinkedList {
+//   constructor(value) {
+//     this.head = {
+//       value: value,
+//       next: null,
+//     };
+//     this.tail = this.head;
+
+//     this.length = 1;
+//   }
+//   append(value){
+//     const newNode= new Node(value);
+//     this.tail.next= newNode;
+//     this.tail = newNode;
+//     this.length++;
+//     return this;
+//   }
+//   prepend(value){
+//       const newNode = new Node(value);
+//       newNode.next= this.head;
+//       this.head =newNode;
+//       this.length++;
+//       return this;
+//   }
+  
+// }
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
   }
-}
-
-class MySinglyLinkedList {
-  constructor(value) {
-    this.head = {
-      value: value,
-      next: null,
-    };
-    this.tail = this.head;
-
-    this.length = 1;
+  
+  class MySinglyLinkedList {
+    constructor(value) {
+      this.head = {
+        value: value,
+        next: null,
+      };
+      this.tail = this.head;
+  
+      this.length = 1;
+    }
+    append(value) {
+      const newNode = new Node(value);
+  
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.length++;
+  
+      return this;
+    }
+    prepend(value) {
+      const newNode = new Node(value);
+  
+      newNode.next = this.head;
+      this.head = newNode;
+  
+      this.length++;
+  
+      return this;
+    }
+    insert(index, value) {
+      if (index >= this.length) {
+        return this.append(value);
+      }
+  
+      const newNode = new Node(value);
+      const firstPointer = this.getTheIndex(index - 1);
+      const holdingPointer = firstPointer.next;
+      firstPointer.next = newNode;
+      newNode.next = holdingPointer;
+  
+      this.length++;
+  
+      return this;
+    }
+  
+    getTheIndex(index) {
+      let counter = 0;
+      let currentNode = this.head;
+  
+      while (counter !== index) {
+        currentNode = currentNode.next;
+        counter++;
+      }
+  
+      return currentNode;
+    }
   }
-}
-
-let myLinkedList = new MySinglyLinkedList(1);
+  
+let myLinkedList = new MySinglyLinkedList(18);
+myLinkedList.append(4);
+myLinkedList.append(5);
+myLinkedList.append(6);
+myLinkedList.append(7);
+myLinkedList.prepend("uno");
+myLinkedList.prepend(0);
