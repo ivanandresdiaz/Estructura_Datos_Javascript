ad0f86b69fc990b066d08ab32c60211f5307b66b
ivan diaz <ivanandresdilox@gmail.com>
2021-01-28 16:14:39 -0500

    listo todo

---
+++ trees/tree.js
@@ -0,0 +1,45 @@
+class Node {
+    constructor(value) {
+      this.left = null;
+      this.right = null;
+      this.value = value;
+    }
+  }
+  
+  class BinarySearchTree {
+    constructor() {
+      this.root = null;
+    }
+    insert(value) {
+      const newNode = new Node(value);
+      if (this.root === null) {
+        this.root = newNode;
+      } else {
+        let currentNode = this.root;
+        while (true) {
+          if (value < currentNode.value) {
+            if (!currentNode.left) {
+              currentNode.left = newNode;
+              return this;
+            }
+            currentNode = currentNode.left;
+          } else {
+            if (!currentNode.right) {
+              currentNode.right = newNode;
+              return this;
+            }
+            currentNode = currentNode.right;
+          }
+        }
+      }
+    }
+  }
+  
+  const tree = new BinarySearchTree();
+  tree.insert(10);
+  tree.insert(4);
+  tree.insert(20);
+  tree.insert(2);
+  tree.insert(8);
+  tree.insert(17);
+  tree.insert(170);
